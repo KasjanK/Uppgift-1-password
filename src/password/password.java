02e2bc811261e13d6e14acdb763c2495a2978fd8
KasjanK <kasol99@gmail.com>
2020-12-04 01:01:27 +0100

    init commit

---
+++ src/password/password.java
@@ -0,0 +1,29 @@
+package password;
+
+import java.util.Scanner;
+
+public class password {
+
+	public static void main(String[] args) {
+		String password = "elefantöra";
+		String pass;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter Your Password:");
+		pass = scanner.nextLine();
+
+		if (pass.equals(password)) {
+			System.out.println("Access granted!");
+		} else {
+			for (int i = 0; i < 2; i++) {
+				System.out.println("Enter Your Password:");
+				pass = scanner.nextLine();
+				if (pass.equals(password)) {
+					System.out.println("Access granted!");
+				}
+			}
+			System.out.println("Access Denied!");
+			scanner.close();
+		}
+	}
+}
